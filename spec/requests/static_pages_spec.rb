18a5d3389a4b6c557201ae2d1aa7015bf9128039
Marcel M. Cary <marcel@oreilly.com>
2014-01-30 12:22:10 -0800

    Use more concise RSpec syntax (Ch. 5.3.4)
    The tutorial suggests using more concise syntax for RSpec to make it
    more readable.
    
    Use the concise syntax, except when the generated messages are less
    informative than the custom "it" string.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,73 +1,47 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  before { visit root_path }
+  subject { page }
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
 
     it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-      expect(page).not_to have_title('|')
+      should_not have_title('| Home')
+      should_not have_title('|')
     end
 
     it "should have layout links" do # extra spec
-      visit root_path
-      expect(page).to have_link("Home", href: "/")
-      expect(page).to have_link("Help", href: "/help")
-      expect(page).to have_link("About", href: "/about")
-      expect(page).to have_link("Contact", href: "/contact")
+      should have_link("Home", href: "/")
+      should have_link("Help", href: "/help")
+      should have_link("About", href: "/about")
+      should have_link("Contact", href: "/contact")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help") }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title("#{base_title} | About Us") }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit contact_path
-      save_page('tmp.html')
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title("#{base_title} | Contact") }
   end
 end
