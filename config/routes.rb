d30b70d0516e4bf1c3dce2c7e2de20ce1fe20afe
Marcel M. Cary <marcel@oreilly.com>
2014-02-20 11:50:40 -0800

    Add Signup Page (Ch. 5.4.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "users/new"
   root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
