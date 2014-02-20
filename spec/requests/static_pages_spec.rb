69f06482a220b98ae35305a1ece790b1f2a642aa
Marcel M. Cary <marcel@oreilly.com>
2014-02-20 12:12:25 -0800

    Visit root path in home page specs only, not all static page specs
    I must have messed this up earlier: I now have the specs visiting '/'
    all the time, even if they are really testing some other page.  For
    other pages, it's an extra page load.
    
    Only visit '/' when that's what we want to test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  before { visit root_path }
   subject { page }
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before { visit root_path }
 
     it { should have_content('Sample App') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
